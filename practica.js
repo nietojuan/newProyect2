d65983bee63030e794a56350314348b9b3022ab8
nietojuan <92448201+nietojuan@users.noreply.github.com>
2023-09-27 08:10:15 -0300

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,15 @@
+/*var miVariable="hola mundo"
+console.log(miVariable); //muestro el valor de la variable.*/
+
+//clase04
+
+
+/*clase5
+//callbacks
+const findPostById = (id, callback) => {
+    const post = posts.find((item) => item.id === id);
+    callback(post);
+    };
+    findPostById(1, (post) => {
+    console.log(post);
+    });*/
